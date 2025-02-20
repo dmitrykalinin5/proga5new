a7e4d5ec5b03bc5388c8ac0e4750d88e70b1a6a2
dmitry <dkalinin240@gmail.com>
2025-02-20 20:35:19 +0300

    Добавлен цикл while true

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,9 @@ import Console.Session;
 
 public class Main {
     public static void main(String[] args) {
+        while (true) {
         Session session = new Session();
         session.run();
+        }
     }
 }
