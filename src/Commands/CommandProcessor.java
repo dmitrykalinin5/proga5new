12a8aaffb44fae4a25a68d777c09adc80004f6b2
dmitry <dkalinin240@gmail.com>
2025-02-20 20:34:46 +0300

    Изменен принцип занесения в словарь, теперь команды вносятся в словарь в зависимости от наличия переменной

--- src/Commands/CommandProcessor.java
+++ src/Commands/CommandProcessor.java
@@ -3,6 +3,7 @@ package Commands;
 import Collections.Coordinates;
 import Collections.TicketType;
 import Validaters.CreationDateValidation;
+import Errors.InvalidInput;
 
 import java.time.LocalDateTime;
 import java.util.HashMap;
@@ -15,15 +16,30 @@ public class CommandProcessor {
         // Словарь
         commands = new HashMap<>();
 
+        // Делим команду, чтобы в будущем посмотреть есть ли переменная
+        String[] parts = name.split(" ");
+
         // Тестовая команда
-        commands.put("test", new TestCommand(123, "Брат",
-                new Coordinates(3, 4), "asd", 10L, TicketType.VIP, "чел"));
-
-        // Список комманд
-        commands.put("help", new HelpCommand());
-
+//        commands.put("test", new TestCommand(123, "Брат",
+//                new Coordinates(3, 4), "asd", 10L, TicketType.VIP, "чел"));
 
         // Выполняем команду, которую ввел пользователь
-        commands.get(name).execute();
+        if (parts.length == 1 && name != null) {
+            // Список команд
+            commands.put("help", new HelpCommand());
+            commands.put("info", new InfoCommand());
+            commands.put("show", new ShowCommand());
+
+            // Смотрим есть ли команда в словаре, выполняем команду
+            if (commands.containsKey(parts[0]))
+            { commands.get(name).execute(); } else System.out.println("Команда не найдена");
+        } else if (parts.length == 2 && parts[0] != null && parts[1] != null) {
+            // Список команд с переменными
+            commands.put("add" + parts[1], new AddCommand(parts[1]));
+
+            // Смотрим есть ли команда в словаре, выполняем команду
+            if (commands.containsKey(parts[0] + parts[1]))
+            { commands.get(parts[0] + parts[1]).execute(); } else System.out.println("Команда не найдена");
+        } else System.out.println("Команда не найдена");
     }
 }
