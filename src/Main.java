93f963b592d16cda23686297588095bb6f58517f
pc <dkalinin240@gmail.com>
2025-02-22 16:28:55 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
 import Console.Session;
+import Commands.ExitCommand;
 
 public class Main {
     public static void main(String[] args) {
         while (true) {
-        Session session = new Session();
-        session.run();
+            Session session = new Session();
+            session.run();
         }
     }
 }
