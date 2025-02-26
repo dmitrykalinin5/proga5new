adba02703da0c92cd16790cb808940cf6a92137f
dmitry <dkalinin240@gmail.com>
2025-02-26 14:41:00 +0300

    Реализация в процессе

--- src/Commands/AddCommand.java
+++ src/Commands/AddCommand.java
@@ -1,5 +1,13 @@
 package Commands;
 
+import Collections.*;
+
+import java.time.LocalDateTime;
+
+/**
+ * Класс, который реализует добавление элемента в коллекцию
+ */
+
 public class AddCommand implements Command {
     private final String element;
 
@@ -10,7 +18,13 @@ public class AddCommand implements Command {
 
     @Override
     public void execute() {
-        System.out.println("Добавить элемент " + element);
+        String[] elements = element.split(",");
+        Coordinates coordinates = new Coordinates(Integer.parseInt(elements[2].split(" ")[0]), Double.parseDouble(elements[2].split(" ")[1]))
+        Person person = new Person(new Zoned elements[6].split(" ")[0]);
+        Ticket ticket = new Ticket(Integer.parseInt(elements[0]), elements[1], coordinates,
+                LocalDateTime.now(), Long.parseLong(elements[4]), TicketType.valueOf(elements[5]), new Person(elements[6]);
+        System.out.printf("Добавить элемент %s\n", element);
+
     }
 
     @Override
