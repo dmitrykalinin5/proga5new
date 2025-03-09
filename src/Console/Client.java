10d616978dbcee89bfb137a0461224b56692c31b
pc <dkalinin240@gmail.com>
2025-03-09 22:56:45 +0300

    collectionManager added

--- src/Console/Client.java
+++ src/Console/Client.java
@@ -2,14 +2,20 @@ package Console;
 
 import java.util.Scanner;
 import Commands.CommandProcessor;
+import Collections.CollectionManager;
 
 public class Client {
+    private final CollectionManager collectionManager;
+
+    public Client(CollectionManager collectionManager) {
+        this.collectionManager = collectionManager;
+    }
 
     public void userInput() {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter command: ");
         String command = scanner.nextLine();
-        CommandProcessor commandProcessor = new CommandProcessor();
+        CommandProcessor commandProcessor = new CommandProcessor(collectionManager);
         commandProcessor.CommandPut();
         /* Обращаемся к commandprocessor, берем метод put, передаем ему команду,
         которую ввел пользователь */
