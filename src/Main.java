975160851959c5ce07c74c9808b0f48b022b99c4
pc <dkalinin240@gmail.com>
2025-03-09 22:57:48 +0300

    CollectionManager added

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
+import Collections.CollectionManager;
 import Console.Session;
-import Commands.ExitCommand;
+import Collections.CollectionManager;
 
 public class Main {
     public static void main(String[] args) {
+        CollectionManager collectionManager = new CollectionManager();
+        collectionManager.loadFromFile();
+        
         while (true) {
-            Session session = new Session();
+            Session session = new Session(collectionManager);
             session.run();
         }
     }
