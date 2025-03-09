f2ecf6d73d07d587f09598330ed51d4ccf84e263
pc <dkalinin240@gmail.com>
2025-03-09 22:58:17 +0300

    CollectionManager

--- src/Console/Session.java
+++ src/Console/Session.java
@@ -1,9 +1,16 @@
 package Console;
 
+import Collections.CollectionManager;
+
 public class Session {
+    private final CollectionManager collectionManager;
+
+    public Session(CollectionManager collectionManager) {
+        this.collectionManager = collectionManager;
+    }
 
     public void run() {
-        Client client = new Client();
+        Client client = new Client(collectionManager);
         client.userInput();
     }
 }
