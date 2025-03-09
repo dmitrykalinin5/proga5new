f2cebd140f9199ad37d9c88e73bd8352f99efcb3
pc <dkalinin240@gmail.com>
2025-03-09 22:58:07 +0300

    execute upd

--- src/Commands/SaveCommand.java
+++ src/Commands/SaveCommand.java
@@ -1,9 +1,18 @@
 package Commands;
 
+import Collections.CollectionManager;
+
 public class SaveCommand implements Command{
+    private final CollectionManager collectionManager;
+
+    public SaveCommand(CollectionManager collectionManager) {
+        this.collectionManager = collectionManager;
+    }
+
     @Override
     public void execute(String[] args) {
         System.out.println("Сохранение...");
+        collectionManager.saveToFile();
     }
 
     @Override
