b7dcf99b483d91db452fdd05d6c179e0e9b79392
dmitry <dkalinin240@gmail.com>
2025-03-10 18:56:16 +0300

    doc

--- src/Commands/Command.java
+++ src/Commands/Command.java
@@ -1,7 +1,15 @@
 package Commands;
 
 public interface Command {
+    /**
+     * Метод, который выполняет команду
+     * @param args
+     */
     void execute(String[] args);
 
+    /**
+     * Метод, который возвращает описание команды
+     * @return String, описание;
+     */
     public String description();
 }
