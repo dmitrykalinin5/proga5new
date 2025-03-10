cb5feeff5f4eed2a3d4e43814ccf1b8fa9e56d0d
dmitry <dkalinin240@gmail.com>
2025-03-10 21:25:31 +0300

    реализован функционал, осталось поймать ошибки

--- src/Commands/ExecuteScriptCommand.java
+++ src/Commands/ExecuteScriptCommand.java
@@ -1,14 +1,69 @@
 package Commands;
 
-public class ExecuteScriptCommand implements Command {
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.io.IOException;
+import java.util.Arrays;
 
-    public ExecuteScriptCommand() {}
+public class ExecuteScriptCommand implements Command {
+    private CommandProcessor commandProcessor;
+
+    public ExecuteScriptCommand(CommandProcessor commandProcessor) {
+        this.commandProcessor = commandProcessor;
+    }
+
+    private String[] readFile(String fileName) {
+        try {
+            String[] content = Files.readString(Path.of(fileName)).split("\n");
+            return content;
+        } catch (NoSuchFileException e) {
+            System.out.println("Файл не найден");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
     @Override
     public void execute(String[] args) {
-        String command = args[0];
         String file_name = args[1];
+        String[] fileInput = readFile(file_name);
+        System.out.println("чо в файле:" + Arrays.toString(fileInput));
+        // Если файл не был найден или произошла ошибка при его чтении, завершаем выполнение метода
+        if (fileInput == null) {
+            return;  // Останавливаем выполнение метода
+        }
+
+        /*
+
+        Нерабочий if снизу, пофиксить
+
+
+         */
+
+
+
+
+        if (fileInput.length == 0) {
+            System.out.println("Файл пустой");
+            return;
+        }
         System.out.println("Запуск скрипта " + file_name + "...");
+        for (String command : fileInput) {
+            command = command.trim();
+            System.out.println("Выполнение команды: " + command.split(" ")[0]);
+            System.out.println(command);
+            commandProcessor.CommandPut();
+            commandProcessor.executeCommand(command);
+        }
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Скрипт выполнен");
     }
 
     @Override
